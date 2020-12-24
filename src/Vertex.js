9045f66483e9355dc12e3b93608d6ed01c4cec0e
Francis-Riggs <59446898+Francis-Riggs@users.noreply.github.com>
2020-12-24 11:24:53 -0500

    add vertexmap and vertex classes

---
+++ src/Vertex.js
@@ -0,0 +1,84 @@
+import { Color, Path } from 'paper';
+
+export default class Vertex {
+    constructor(point, coordinates, tileRadius) {
+        this.id = `${coordinates[0]}-${coordinates[1]}-V`;
+        this.center = point;
+        this.coordinates = coordinates;
+        this.tileRadius = tileRadius;
+        this.setClickBoundary(this.tileRadius * 0.6);
+    }
+
+    setClickBoundary(circleRadius) {
+        if (this.clickBoundary) this.clickBoundary.remove();
+        this.clickBoundary = new Path.Circle({
+            center: this.center,
+            strokeColor: new Color(0, 0, 255, 0.01),
+            fillColor: new Color(0, 0, 255, 0.01),
+            radius: circleRadius,
+            opacity: 0.1
+        });
+    }
+
+    setReferences(vertexMap, tileGrid) {
+        this.setAdjacentVertices(vertexMap);
+        this.setAdjacentTiles(tileGrid);
+        this.setClickBoundary(this.tileRadius / 2);
+        this.attachEventListeners();
+    }
+
+    setAdjacentVertices(vertexMap) {
+        const vertices = [];
+        for (const vertexCol of vertexMap.vertices) {
+            for (const vertex of vertexCol) {
+                if (vertex.id !== this.id) {
+                    const intersections = this.clickBoundary.getIntersections(vertex.clickBoundary);
+                    if (intersections.length) {
+                        vertices.push(vertex);
+                    }
+                }
+            }
+        }
+        this.adjacentVertices = vertices;
+    }
+
+    setAdjacentTiles(tileGrid) {
+        const tiles = [];
+        for (const tileCol of tileGrid.tiles) {
+            for (const tile of tileCol) {
+                const intersections = this.clickBoundary.getIntersections(tile.hex.path);
+                if (intersections.length) {
+                    tiles.push(tile);
+                }
+            }
+        }
+        this.adjacentTiles = tiles;
+    }
+
+    attachEventListeners() {
+        this.clickBoundary.onMouseEnter = this.handleMouseEnter.bind(this);
+        this.clickBoundary.onMouseLeave = this.handleMouseLeave.bind(this);
+        this.clickBoundary.onMouseDown = this.handleMouseDown.bind(this);
+        this.clickBoundary.onMouseUp = this.handleMouseUp.bind(this);
+    }
+
+    handleMouseEnter() {
+        this.clickBoundary.opacity = 1;
+        this.clickBoundary.strokeColor = new Color(0, 0, 255, 1);
+    }
+
+    handleMouseLeave() {
+        this.clickBoundary.opacity = 0;
+        this.clickBoundary.strokeColor = new Color(0, 0, 255, 0.01);
+    }
+
+    handleMouseDown() {
+        this.clickBoundary.tweenTo({ fillColor: new Color(0, 0, 255, 0.5) }, 100);
+        console.log(this.adjacentVertices);
+        console.log(this.adjacentTiles);
+    }
+
+    handleMouseUp() {
+        this.clickBoundary.tweenTo({ fillColor: new Color(0, 0, 255, 0.1) }, 100);
+    }
+}
---
+++ src/VertexMap.js
@@ -0,0 +1,44 @@
+import Vertex from './Vertex';
+
+export default class VertexMap {
+    constructor(tileGrid) {
+        this.vertices = [];
+        this.generateVertexMap(tileGrid);
+        this.setupMapReferences(tileGrid);
+    }
+
+    generateVertexMap(tileGrid) {
+        let isLeftHalf = true;
+        const vertexCols = [];
+        const tileRadius = tileGrid.radius;
+        for (let i = 0; i < tileGrid.tiles.length; i++) {
+            const col = [];
+            for (let j = 0; j < tileGrid.tiles[i].length; j++) {
+                const tile = tileGrid.tiles[i][j];
+                const hexPoints = tile.hex.getPoints();
+                if (isLeftHalf) {
+                    if (j === 0) col.push(new Vertex(hexPoints[0], [this.vertices.length, col.length], tileRadius));
+                    col.push(new Vertex(hexPoints[5], [this.vertices.length, col.length], tileRadius));
+                    col.push(new Vertex(hexPoints[4], [this.vertices.length, col.length], tileRadius));
+                } else {
+                    if (j === 0) col.push(new Vertex(hexPoints[1], [this.vertices.length, col.length], tileRadius));
+                    col.push(new Vertex(hexPoints[2], [this.vertices.length, col.length], tileRadius));
+                    col.push(new Vertex(hexPoints[3], [this.vertices.length, col.length], tileRadius));
+                }
+            }
+            this.vertices.push(col);
+            if (isLeftHalf) {
+                isLeftHalf = i < Math.floor(tileGrid.tiles.length / 2);
+                if (!isLeftHalf) i--;
+            }
+        }
+    }
+
+    setupMapReferences(tileGrid) {
+        for (const vertexCol of this.vertices) {
+            for (const vertex of vertexCol) {
+                vertex.setReferences(this, tileGrid);
+            }
+        }
+    }
+}
